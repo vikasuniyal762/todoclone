029bef725244cc0ce999d3729ec18de1635a9a6f
Vikas Uniyal <vikasuniyal762@gmail.com>
2023-08-24 18:01:48 +0530

    Initial commit only add and delete item with check box 24-08-2023

---
+++ lib/architecturefiles/appColors.dart
@@ -0,0 +1,27 @@
+// import 'package:flutter/material.dart';
+//
+// class AppColor {
+//   static const Color primaryColor = Color(0xff8bc34b);
+//   static const Color primaryColorOpacity25 = Color(0x808bc34b);
+//   static const Color secondaryColor = Color(0xff4384aa);
+//
+//   static const Color primaryColorAccent = Color(0xff8bc34b);
+//   static const Color secondaryColorAlt = Color(0xffe0e4e7);
+//   static const Color tertiaryColor = Colors.blueGrey;
+//   static Color teal = Colors.teal.shade200;
+//   static const Color teal400 = Color(0xFF26A69A);
+//
+//
+//   static const Color transparent = Colors.transparent;
+//   static const Color white = Color(0xffffffff);
+//   static const Color black = Colors.black;
+//   static const Color blackMild = Colors.black54;
+//   static const Color grey = Colors.grey;
+//   static const Color greyLight = Color(0xffE8e8e8);
+//   static const Color greyShimmer = Color(0xffbdbdbdb);
+//   static const Color red = Colors.red;
+//   static const Color green = Colors.green;
+//   static const Color blue = Colors.blue;
+//   static const Color shade = Color(0xa82aaf1d);
+//   static const Color shadefade = Color(0x9079E56F);
+// }
---
+++ lib/architecturefiles/screenSize.dart
@@ -0,0 +1,6 @@
+// import 'package:flutter/material.dart';
+//
+// class ScreenSize {
+//   static double height(context) => MediaQuery.of(context).size.height;
+//   static double width(context) => MediaQuery.of(context).size.width;
+// }
